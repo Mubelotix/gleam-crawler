e714ad79b7085a8dbef9f80cb7164cdce3889584
Mubelotix <unbeliever.in.time@gmail.com>
2019-11-09 12:17:54 +0100

    added progress bar and timeout

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use gleam_finder::*;
 use std::thread;
 use std::time::Duration;
+use std::env;
+use progress_bar::progress_bar::ProgressBar;
+use progress_bar::color::{Color, Style};
 use clap::*;
 
 fn main() {
@@ -21,7 +24,16 @@ fn main() {
                 .min_values(0)
                 .max_values(86400)
                 .default_value("6")
-                .help("Set set waiting time in seconds between two request to the same website.")
+                .help("Set the waiting time in seconds between two request to the same website.")
+        )
+        .arg(
+            Arg::with_name("timeout")
+                .long("timeout")
+                .takes_value(true)
+                .min_values(0)
+                .max_values(3600)
+                .default_value("6")
+                .help("Set the timeout for a request.")
         )
         .get_matches();
 
@@ -32,20 +44,39 @@ fn main() {
     };
 
     let cooldown: u64 = matches.value_of("cooldown").unwrap_or("6").parse().unwrap_or(6);
+    env::set_var("MINREQ_TIMEOUT", matches.value_of("timeout").unwrap_or("6"));
 
-    for page in 0..4 {
-        if !minimal { println!("\x1B[0;34mloading google page {}", page); }
-        for link in google::search(page) {
-            if !minimal { println!("\x1B[0;34mloading {}", link); }
-            for gleam_link in intermediary::resolve(&link) {
-                if !minimal {
-                    println!("\x1B[1;32mgleam link found: {}", gleam_link);
-                } else {
-                    println!("{}", gleam_link);
-                }
+    if !minimal {
+        let mut progress_bar = ProgressBar::new(10);
+        progress_bar.set_action("Searching", Color::White, Style::Normal);
+
+        let mut results: Vec<String> = Vec::new();
+        let mut page = 0;
+        loop {
+            progress_bar.print_info("Getting", &format!("the results page {}", page), Color::Blue, Style::Normal);
+            let mut new_results = google::search(page);
+            if new_results.len() > 0 {
+                results.append(&mut new_results);
+                page += 1;
+                progress_bar.inc();
+                progress_bar.print_info("Sleeping", &format!("for {} seconds", cooldown), Color::Yellow, Style::Normal);
+                thread::sleep(Duration::from_secs(cooldown));
+            } else {
+                break;
             }
-            if !minimal { println!("\x1B[0;33msleeping"); }
+        }
+
+        let mut progress_bar = ProgressBar::new(results.len());
+        progress_bar.set_action("Loading", Color::White, Style::Normal);
+        for link in results {
+            progress_bar.print_info("Loading", &link, Color::Blue, Style::Normal);
+            for gleam_link in intermediary::resolve(&link) {
+                progress_bar.print_info("Found", &gleam_link, Color::LightGreen, Style::Bold);
+            }
+            progress_bar.inc();
+            progress_bar.print_info("Sleeping", &format!("for {} seconds", cooldown), Color::Yellow, Style::Normal);
             thread::sleep(Duration::from_secs(cooldown));
         }
     }
+    
 }
